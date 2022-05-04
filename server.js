9f54d6720c0e38fcd156923fb1a85ee92355fe58
Sangkun-svg <gims4752@gmail.com>
2022-05-04 14:32:53 +0900

    feat : 유저의 목록을 가져오는 api 구현

--- server.js
+++ server.js
@@ -1,14 +1,57 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 
 const port = process.env.PORT || 9000;
+const options = {
+  allowedHeaders: [
+    "*",
+    "Origin",
+    "X-Requested-With",
+    "X-Custom-Header",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token",
+    "authorization",
+  ],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "*",
+  preflightContinue: false,
+};
 
+app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello express !" });
+app.get("/api/customers", (req, res) => {
+  console.log("get all customers");
+  return res.send([
+    {
+      id: 1,
+      image: "https://placeImg.com/64/64/1",
+      name: "Sangkun",
+      age: 23,
+      gender: "male",
+      career: "GnB",
+    },
+    {
+      id: 2,
+      image: "https://placeImg.com/64/64/2",
+      name: "Tay",
+      age: 30,
+      gender: "male",
+      career: "GnB",
+    },
+    {
+      id: 3,
+      image: "https://placeImg.com/64/64/3",
+      name: "Summer",
+      age: 32,
+      gender: "female",
+      career: "GnB",
+    },
+  ]);
 });
 
 app.listen(9000, () => {
