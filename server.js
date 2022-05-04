81d2edce76c340db77289dd33f15aac52b893b44
Sangkun-svg <gims4752@gmail.com>
2022-05-05 02:04:13 +0900

    feat : customer 추가 api 구현

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { dbConfig } from "./sequelize.js";
 import { Customers } from "./user.model.js";
+import multer from "multer";
 const app = express();
 
 const port = process.env.PORT || 9000;
@@ -21,15 +22,37 @@ const options = {
   origin: "*",
   preflightContinue: false,
 };
-
+const upload = multer({ dest: "./upload" });
 app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/image", express.static("./upload"));
 
 app.get("/api/customers", async (req, res) => {
   try {
     const customers = await Customers.findAll({ raw: true });
     return res.send(customers);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post("/api/customers", upload.single("image"), async (req, res) => {
+  try {
+    const {
+      body: { name, age, gender, career },
+      file: { filename },
+    } = req;
+    const image = "/image/" + filename;
+    const newCustomer = {
+      name: name,
+      age: age,
+      gender: gender,
+      career: career,
+      image: image,
+    };
+    const addCustomer = await Customers.create(newCustomer);
+    return;
   } catch (err) {
     console.error(err);
   }
