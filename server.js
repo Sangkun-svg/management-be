faf562737ab0325acf6cb6a35afaf99433456d49
Sangkun-svg <gims4752@gmail.com>
2022-05-09 01:17:23 +0900

    feat: 모듈화 및 router-controller-service 패턴 도입 을 위해 server.js 의 비즈니스 로직 삭제

--- server.js
+++ server.js
@@ -29,43 +29,7 @@ app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/image", express.static("./upload"));
-// app.use("/api/user", userRouter);
-
-app.post("/api/user/register", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    const isRegistered = true;
-    return isRegistered;
-  } catch (error) {
-    console.error(error);
-    throw new Error("user register Error");
-  }
-});
-
-app.post("/api/user/login", async (req, res) => {
-  try {
-    let isUser = false;
-    console.log("req.body : ", req.body);
-    const { id, password } = req.body;
-    const user = await User.findOne({
-      where: {
-        id: id,
-      },
-    });
-    if (!user) {
-      res.send("존재하지 않는 아이디입니다. 다시 입력해주세요.");
-      throw new Error("존재하지 않는 아이디입니다. 다시 입력해주세요.");
-    }
-    if (user.password !== password) {
-      res.send("존재하지 않는 비밀번호입니다. 다시 입력해주세요.");
-      throw new Error("존재하지 않는 비밀번호입니다. 다시 입력해주세요.");
-    }
-    console.log(isUser);
-    return res.send("로그인 성공 !");
-  } catch (error) {
-    console.log(error);
-  }
-});
+app.use("/api/user", userRouter);
 
 app.listen(9000, () => {
   console.log(`Listening server port ${port}`);
