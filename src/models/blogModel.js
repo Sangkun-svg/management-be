81e971dcd95c553fe16b1e17323dec95d6db6735
Sangkun-svg <gims4752@gmail.com>
2022-05-24 23:21:45 +0900

    feat : generate blog model

---
+++ src/models/blogModel.js
@@ -0,0 +1,36 @@
+import * as Sequelize from "sequelize";
+import { dbConfig } from "../../sequelize.js";
+
+const { INTEGER, STRING, DATE, NOW, BOOLEAN } = Sequelize;
+export const Blog = dbConfig.define("blog", {
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    type: INTEGER,
+  },
+  title: {
+    allowNull: false,
+    type: STRING,
+  },
+  writer: {
+    allowNull: false,
+    type: STRING,
+  },
+  content: {
+    allowNull: false,
+    type: STRING,
+  },
+  is_deleted: {
+    defaultValue: 0,
+    type: BOOLEAN,
+  },
+  created_at: {
+    type: DATE,
+    defaultValue: NOW,
+  },
+  updated_at: {
+    type: DATE,
+    defaultValue: NOW,
+  },
+});
